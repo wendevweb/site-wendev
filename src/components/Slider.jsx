608bf09c0688742457c9e653de1977a6d8c34078
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-02-16 10:07:33 +0100

    Commit to checkout

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,54 +1,49 @@
 import React from "react";
-import "./slider.scss";
-import nelixair from "../assets/logo_full_assoc.webp";
-import kodework from "../assets/kodeworklogo.webp";
-import rookies from "../assets/rookies.webp";
-import antiigone from "../assets/antiigone.webp";
+import "./slider.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const Slider = () => {
+  const datas = [
+    {
+      id: 1,
+      image:
+        "https://www.hebergeur-image.com/upload/88.174.47.6-63e6579c17715.png",
+    },
+    {
+      id: 2,
+      image:
+        "https://www.hebergeur-image.com/upload/88.174.47.6-63e657c94211a.png",
+    },
+    {
+      id: 3,
+      image:
+        "https://www.hebergeur-image.com/upload/88.174.47.6-63edf1d36caca.png",
+    },
+  ];
   return (
-    <div className="slider-body">
+    <div className="lg:m-10">
       <h3 className="text-pink text-2xl text-center mt-12 mb-6">
         Ils me font confiance
       </h3>
-      <div className="slider">
-        <div className="slide-track p-4">
-          <div className="slide">
-            <img
-              src={nelixair}
-              height="200"
-              width="250"
-              alt="Association Nelixair"
-            />
+
+      <Carousel
+        autoPlay
+        interval={3000}
+        infiniteLoop
+        thumbWidth={120}
+        showIndicators={true}
+        showStatus={false}
+        autoFocus={false}
+        dynamicHeight={true}
+      >
+        {datas.map((slide) => (
+          <div key={slide.id} aria-label="string" className="bg-light">
+            <img src={slide.image} alt="" className="carousel-img " />
           </div>
-          <div className="slide p-4">
-            <img
-              src={kodework}
-              height="100"
-              width="250"
-              alt="Plateforme KodeWork"
-            />
-          </div>
-          <div className="slide p-4">
-            <img
-              src={rookies}
-              height="100"
-              width="250"
-              alt="Plateforme Rookies"
-            />
-          </div>
-          <div className="slide p-4">
-            <img
-              src={antiigone}
-              height="50"
-              width="100"
-              alt="Wendy Baqué, auteure de romans"
-            />
-          </div>
-        </div>
-      </div>
+        ))}
+      </Carousel>
     </div>
   );
 };
-
 export default Slider;
